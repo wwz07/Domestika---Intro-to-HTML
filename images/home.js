a4e370e80d6f8dc71e32fb234b3e737d6020d6ea
wirazulka <willy.wirawan1@gmail.com>
2021-12-13 21:58:25 +0700

    javascript interaction

---
+++ images/home.js
@@ -0,0 +1,30 @@
+/* Variables: change the variables for the corresponding id and class*/
+
+/* id of the link that display in the menu button */
+var popper ="#menu-popper";
+
+/* id of the menu that will be displayed */
+var unfold="#menu";
+
+/*class (without the dot) that shows the menu*/
+var display_class="navigation";
+
+/* From here you can leave the code as is*/
+
+/*Declare the function*/
+function nav(){
+    var popping=document.querySelector(popper);
+    popping.addEventListener("click",displayMenu,false);
+}
+
+function displayMenu(e){
+    e.preventDefault();
+    var deploy=document.querySelector(unfold);
+    deploy.classList.toggle(display_class);
+}
+
+/* Add the js class to the html tag to know that js file is working*/
+document.querySelector("html").classList.add("js");
+
+/*execute the main function*/
+nav();
